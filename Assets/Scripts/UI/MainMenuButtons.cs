f20e2aaf27622ddd1e45cb295ada308eab2a10dd
Ekin Karıncalı <s230446019@stu.thk.edu.tr>
2026-05-22 15:01:54 +0300

    feat: play button with save system integration

---
+++ Assets/Scripts/UI/MainMenuButtons.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI playButtonText;
+
+    private void Start()
+    {
+        UpdatePlayButtonLabel();
+    }
+    private void UpdatePlayButtonLabel()
+    {
+        int lastLevel = SaveSystem.LoadLastLevel();
+        playButtonText.text = lastLevel == 0 ? "New Game" : "Continue";
+    }
+
+    public void OnPlayClicked()
+    {
+        // int lastLevel = SaveSystem.LoadLastLevel();
+        // if (lastLevel == 0)
+        //     SceneManager.LoadScene("SampleScene");
+        // else
+        //     SceneManager.LoadScene(lastLevel);
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OnMissionsClicked()
+    {
+        Debug.Log("Missions clicked");
+    }
+
+    public void OnCreditsClicked()
+    {
+        Debug.Log("Credits clicked");
+    }
+
+    public void OnSettingsClicked()
+    {
+        Debug.Log("Settings clicked");
+    }
+
+    public void OnExitClicked()
+    {
+        Debug.Log("Exit clicked");
+    }
+}
