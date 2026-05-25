5471cdc4b8b980240aa585e3b3884bd8ac9ae4e3
BerdanG <berdangencer@yahoo.com>
2026-05-25 13:46:49 +0300

    wip: continue missile explosion system

--- Assets/Scripts/Missile/Missile.cs
+++ Assets/Scripts/Missile/Missile.cs
@@ -8,15 +8,31 @@ public class Missile : MonoBehaviour
     [Header("Lifetime")]
     [SerializeField] private float lifeTime = 5f;
 
-    private Rigidbody rb;
+    [Header("Explosion")]
+    [SerializeField] private GameObject explosionVfx;
 
+    private Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
         rb.linearVelocity = transform.forward * speed;
 
         Destroy(gameObject, lifeTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Explode();
+    }
+
+    void Explode()
+    {
+        if (explosionVfx != null)
+        {
+            Instantiate(explosionVfx, transform.position, Quaternion.identity);
+        }
+
+        Destroy(gameObject);
     }
 }
